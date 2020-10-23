a83e48f887ea2510c087b52984530f2a038da156
Maniek <mariusz.aleszko@gmail.com>
2020-10-23 18:20:16 +0200

    deploy

---
+++ precache-manifest.1c64eff9459028508c9b81844c499e9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf770451c26a56a88b9cfe07cd0c670",
+    "url": "/colors/index.html"
+  },
+  {
+    "revision": "a842eb528ed91b2ed9dd",
+    "url": "/colors/static/js/2.0a5f92d5.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/colors/static/js/2.0a5f92d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e138f2def19066cf51ce",
+    "url": "/colors/static/js/main.013eabfd.chunk.js"
+  },
+  {
+    "revision": "b21169911627de49c135",
+    "url": "/colors/static/js/runtime-main.4f478495.js"
+  }
+]);
